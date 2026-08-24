4a12afb566c8e9ba36fcf8ab5deccb3002cb93d4
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2026-08-24 21:19:37 +0200

    added checkstyle dependencies and formatted class name

--- src/main/java/org/coolplugins/cool_HealthBar/Coolhealthbar.java
+++ src/main/java/org/coolplugins/cool_HealthBar/Coolhealthbar.java
@@ -2,7 +2,7 @@ package org.coolplugins.cool_HealthBar;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Cool_HealthBar extends JavaPlugin {
+public final class Coolhealthbar extends JavaPlugin {
 
     @Override
     public void onEnable() {
