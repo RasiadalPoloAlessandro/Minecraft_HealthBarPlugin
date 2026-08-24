1537666f1b6f30a3bbbc05561450f4ffd40c5bfb
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2026-08-24 22:30:18 +0200

    added simple log message

--- src/main/java/org/coolplugins/cool_HealthBar/Coolhealthbar.java
+++ src/main/java/org/coolplugins/cool_HealthBar/Coolhealthbar.java
@@ -7,6 +7,8 @@ public final class Coolhealthbar extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        getLogger().info("CoolHealthBar e' stato avviato correttamente!");
     }
 
     @Override
