922cd8fbe93f263ceb69832608135acf64b21554
RasiadalPoloAlessandro <ale.rasiadalpolo12@gmail.com>
2026-09-07 17:15:24 +0200

    refactor(formatter): extract health bar rendering into dedicated class

---
+++ src/main/java/org/coolplugins/cool_HealthBar/gui/HealthBar.java
@@ -0,0 +1,69 @@
+package org.coolplugins.cool_HealthBar.gui;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.entity.LivingEntity;
+
+public class HealthBar {
+
+    protected static final TextColor FULL_HEALTH_COLOR = TextColor.color(0, 255, 0);
+    protected static final TextColor HALF_HEALTH_COLOR = TextColor.color(255,255,0);
+    protected static final TextColor LOW_HEALTH_COLOR = TextColor.color(255,0,0);
+    private final int totalSegments;
+    private final char filledChar;
+    private final char emptyChar;
+    private final TextColor emptyColor;
+    private final TextColor bracketColor;
+
+
+    public HealthBar() {
+        this(10, '▰', '▱', TextColor.color(60, 60, 60), TextColor.color(120, 120, 120));
+    }
+
+    public HealthBar(int totalSegments, char filledChar, char emptyChar, TextColor emptyColor, TextColor bracketColor) {
+        this.totalSegments = totalSegments;
+        this.filledChar = filledChar;
+        this.emptyChar = emptyChar;
+        this.emptyColor = emptyColor;
+        this.bracketColor = bracketColor;
+    }
+
+
+    /**
+     *
+     * @param percentage health expressed in percentage
+     * @return the health bar formed by specified chars
+     */
+    public Component buildBarComponent(double percentage) {
+
+        int filledSegments = (int) Math.round((percentage / 100) * totalSegments);
+        if (percentage > 0 && filledSegments == 0)
+            filledSegments = 1;
+
+        TextComponent.Builder bar = Component.text();
+        TextColor textColor;
+
+        if(percentage > 50)
+            textColor = FULL_HEALTH_COLOR;
+        else if(percentage <= 50 && percentage > 20)
+            textColor = HALF_HEALTH_COLOR;
+        else
+            textColor = LOW_HEALTH_COLOR;
+
+        for(int i = 0; i < filledSegments; i++)
+            bar.append(Component.text(filledChar, textColor));
+
+        for(int i = filledSegments; i < totalSegments; i++)
+            bar.append(Component.text(emptyChar, emptyColor));
+
+        return Component.text()
+                .append(Component.text("[", bracketColor))
+                .append(bar.build())
+                .append(Component.text("] ", bracketColor))
+                .append(Component.text(String.format("%.0f%%", percentage)))
+                .build();
+    }
+
+
+}
--- src/main/java/org/coolplugins/cool_HealthBar/gui/healthformatter/EntityHealthBarFormatter.java
+++ src/main/java/org/coolplugins/cool_HealthBar/gui/healthformatter/EntityHealthBarFormatter.java
@@ -8,19 +8,12 @@ import org.bukkit.entity.Horse;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.coolplugins.cool_HealthBar.HealthManager;
+import org.coolplugins.cool_HealthBar.gui.HealthBar;
 import org.coolplugins.cool_HealthBar.pdc.MobNamePDC;
 
 public abstract class EntityHealthBarFormatter implements HealthBarLabel {
 
 
-    protected static final TextColor FULL_HEALTH_COLOR = TextColor.color(0, 255, 0);
-    protected static final TextColor HALF_HEALTH_COLOR = TextColor.color(255,255,0);
-    protected static final TextColor LOW_HEALTH_COLOR = TextColor.color(255,0,0);
-    protected static final int TOTAL_SEGMENTS = 10;
-    protected static final char FILLED_CHAR = '▰';
-    protected static final char EMPTY_CHAR = '▱';
-    protected static final TextColor EMPTY_COLOR = TextColor.color(60, 60, 60);
-    protected static final TextColor BRACKET_COLOR = TextColor.color(120, 120, 120);
     private final MobNamePDC mobNamePDC;
 
     public EntityHealthBarFormatter(MobNamePDC mobNamePDC) {
@@ -35,41 +28,6 @@ public abstract class EntityHealthBarFormatter implements HealthBarLabel {
      */
     protected Component getEntityHeader(LivingEntity livingEntity) { return mobNamePDC.getEffectiveName(livingEntity);}
 
-    /**
-     *
-     * @param livingEntity entity
-     * @param percentage health expressed in percentage
-     * @return the health bar formed by specified chars
-     */
-    private Component buildBarComponent(LivingEntity livingEntity, double percentage) {
-
-        int filledSegments = (int) Math.round((percentage / 100) * TOTAL_SEGMENTS);
-        if (percentage > 0 && filledSegments == 0)
-            filledSegments = 1;
-
-        TextComponent.Builder bar = Component.text();
-        TextColor textColor;
-
-        if(percentage > 50)
-            textColor = FULL_HEALTH_COLOR;
-        else if(percentage <= 50 && percentage > 20)
-            textColor = HALF_HEALTH_COLOR;
-        else
-            textColor = LOW_HEALTH_COLOR;
-
-        for(int i = 0; i < filledSegments; i++)
-            bar.append(Component.text(FILLED_CHAR, textColor));
-
-        for(int i = filledSegments; i < TOTAL_SEGMENTS; i++)
-            bar.append(Component.text(EMPTY_CHAR, EMPTY_COLOR));
-
-        return Component.text()
-                .append(Component.text("[", BRACKET_COLOR))
-                .append(bar.build())
-                .append(Component.text("] ", BRACKET_COLOR))
-                .append(Component.text(String.format("%.0f%%", percentage)))
-                .build();
-    }
 
     /**
      *
@@ -79,13 +37,13 @@ public abstract class EntityHealthBarFormatter implements HealthBarLabel {
     @Override
     public Component format(LivingEntity entity) {
 
+        HealthBar bar = new HealthBar();
         double percentage = HealthManager.getPercentage(entity);
-        Component bar = buildBarComponent(entity, percentage);
         Component mobHeader = getEntityHeader(entity);
 
         TextComponent.Builder builder = Component.text();
 
-        builder.append(bar);
+        builder.append(bar.buildBarComponent(percentage));
         // It's a mob whose name has been memorized
         // if it's null it means the entity is a Player, so the name will automatically appear
         if(mobHeader != null)
